4c2f1848a84f06551dd8a977d7e12c92cdbc6d85
Ver Caparas <ver.caparas@betterask.erni>
2026-07-23 01:41:52 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using KimberlinViado.Portfolio.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+app.Run();
